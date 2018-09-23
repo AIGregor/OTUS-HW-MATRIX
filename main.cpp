171a4c676b667b8ae636cb9768e91d4b5b1c55d9
AIGregor <real.garik@gmail.com>
2018-09-23 15:08:13 +0300

    add: gtest to cmakelist

--- main.cpp
+++ main.cpp
@@ -1,56 +1,14 @@
 #pragma once
 #include <iostream>
-#include <tuple>
 #include <cassert>
-
 #include "Matrix.h"
 
 #define MAX_ZISE 9
 
-void test() {
-
-	Matrix<int, -1> matrix; // бесконечная матрица int заполнена значениями -1
-
-	assert(matrix.size() == 0); // все ячейки свободны
-
-	auto a = matrix[0][0];
-
-	assert(a == -1);
-	assert(matrix.size() == 0);
-
-	matrix[100][100] = 314;
-
-	assert(matrix[100][100] == 314);
-	assert(matrix.size() == 1);
-
-	matrix[100][10] = 457;
-
-	assert(matrix[100][10] == 457);
-	assert(matrix.size() == 2);
-
-	matrix[100][100] = -1;
-	assert(matrix[100][100] == -1);
-	assert(matrix.size() == 1);
-
-	// выведется одна строка
-	// 100100314
-	for (auto c : matrix)
-	{
-		int x = c.first;
-		for (auto e : c.second)
-		{
-			int y = e.first;
-			int v = e.second.getValue();
-			//std::tie(x, y, v) = c;
-			std::cout << x << y << v << std::endl;
-		}
-	}
-}
-
 int main()
 {
-	Matrix<int, 0> matrix; // бесконечная матрица int заполнена значениями -1
-
+	Matrix<int, 0> matrix;
+	
 	// main Diagonal
 	int i = 0;
 	while (i <= MAX_ZISE)
@@ -66,6 +24,7 @@ int main()
 		--i;
 	}
 
+	// print matrix 8x8
 	for (int i = 1; i < MAX_ZISE; ++i)
 	{
 		for (int j= 1; j < MAX_ZISE; ++j)
